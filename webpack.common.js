102d118c211796e55b3efcf2ff804a535f1ddd46
DwkLoki <dwiky.darmawansyah@gmail.com>
2022-06-28 00:23:15 +0700

    ubah path service worker

--- webpack.common.js
+++ webpack.common.js
@@ -55,7 +55,7 @@ module.exports = {
       ]
     }),
     new ServiceWorkerWebpackPlugin({
-      entry: path.resolve(__dirname, 'src/scripts/sw.js')
+      entry: path.resolve(__dirname, './src/scripts/sw.js')
     }),
     new ImageminWebpackPlugin({
       plugins: [
